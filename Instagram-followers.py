992f4fbb0afcf9f06b4d225f62f87b6209d00f4c
Pragya Mittal <69578414+pragyamittal0@users.noreply.github.com>
2021-06-02 10:54:53 +0530

    Update Instagram-followers.py

--- Instagram-followers.py
+++ Instagram-followers.py
@@ -1,5 +1,5 @@
 from instabot import Bot
 bot= Bot()
 
-bot.login(username="_pragya_mittal181_", password="gautam123")
-bot.follow("ujjwal.incognito")
+bot.login(username="_pragya_mittal181_", password="*******")
+bot.follow("*****")
